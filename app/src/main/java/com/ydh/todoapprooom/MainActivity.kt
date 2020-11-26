74ad21b5c918e25c3f321178f58cafa21f6491f8
“hiwansyah” <iwansyy@gmail.com>
2020-11-26 09:11:55 +0700

    ui commit

--- app/src/main/java/com/ydh/todoapprooom/MainActivity.kt
+++ app/src/main/java/com/ydh/todoapprooom/MainActivity.kt
@@ -2,10 +2,45 @@ package com.ydh.todoapprooom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.viewpager2.widget.ViewPager2
+import com.ydh.todoapprooom.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    lateinit var menuItem: MenuItem
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setupViewPager()
+        binding.navTodo.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.menuTodo -> binding.vpTodo.currentItem = 0
+                R.id.menuFavorite -> binding.vpTodo.currentItem = 1
+                R.id.menuProfile -> binding.vpTodo.currentItem = 2
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
+        setContentView(binding.root)
+    }
+
+    private fun setupViewPager() {
+        binding.run {
+            val pages = listOf(
+                Page("Todo", TodoFragment()),
+                Page("Favorite", FavoritesFragment()),
+                Page("Profile", ProfileFragment()))
+            val adapter = ViewPagerAdapter(pages, supportFragmentManager, lifecycle)
+            vpTodo.adapter = adapter
+
+            vpTodo.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    navTodo.menu.getItem(0).isChecked = false
+                    navTodo.menu.getItem(position).isChecked = true
+                    menuItem = navTodo.menu.getItem(position)
+                }
+            })
+        }
     }
 }
